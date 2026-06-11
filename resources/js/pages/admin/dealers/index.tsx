0ad5cd41164152c369f22318f78f6ac3b7bf84fd
Daffa Akbar <daffa.akbar@mpm-motor.com>
2026-06-11 11:50:56 +0700

    feat: import DealerController for enhanced dealer management functionality

--- resources/js/pages/admin/dealers/index.tsx
+++ resources/js/pages/admin/dealers/index.tsx
@@ -7,6 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Badge } from '@/components/ui/badge';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import * as DealerController from '@/actions/App/Http/Controllers/Admin/DealerController';
 import { LuPencil, LuTrash2, LuPlus, LuMapPin, LuInfo } from 'react-icons/lu';
 
 interface Dealer {
